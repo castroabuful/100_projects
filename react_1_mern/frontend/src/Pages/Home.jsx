d016e4f71755dbecaa46ea36073ae20c13badd44
castroabuful <castro.173scp@gmail.com>
2026-04-12 12:10:20 +0300

    front_end_adjustments_2

--- react_1_mern/frontend/src/Pages/Home.jsx
+++ react_1_mern/frontend/src/Pages/Home.jsx
@@ -2,15 +2,10 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 
 const Home = () => {
-
-
-
-
   return (
     <>
-    <Navbar/>
-    <div className='w-screen h-screen'>
-
+    <div className='min-h-screen bg-black'>
+      <Navbar/>
     </div>
     </>
   )
--- react_1_mern/frontend/src/components/Navbar.jsx
+++ react_1_mern/frontend/src/components/Navbar.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import {PlusIcon} from "lucide-react"
+import { Link } from 'lucide-react'
 
 const Navbar = () => {
   return (
-  <div className=''>
+  <nav className='flex items-center justify-between px-6 h-14 bg-[oklch(21%_0.034_264.665)] border-b border-green-900 sticky top-0 z-50'>
+  
+   <h1 className='text-green-400 text-xl font-bold font-mono tracking-tight'>
+    ThinkBoard
+   </h1>
 
-  </div>
+   <Link to="/create" className='flex items-center gap-2 bg-green-500 hover:bg-green-400 text-black font-medium px-4 py-2 rounded-full text-sm transition-colors'>
+     <PlusIcon className='size-4'/>
+     New Note
+   </Link>
+
+  </nav>
   )
 }
 
